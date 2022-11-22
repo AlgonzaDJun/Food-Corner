c2a8cea95544bece1cd1eb95fcb3b9307e8268ff
AlgonzaDJun <algonza.20050@mhs.unesa.ac.id>
2022-11-22 08:23:06 +0700

    order

---
+++ frontend/src/actions/orderActions.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+export const placeOrder = (token, totalPrice) => async (dispatch, getState) => {
+  //   var items = [];
+
+  dispatch({
+    type: "PLACE_ORDER_REQUEST",
+  });
+  const currentUser = getState().loginUserReducer.currentUser;
+  const cartItems = getState().cartReducer.cartItems;
+
+  try {
+    const response = await axios.post("/api/orders/placeorders", {
+      token,
+      totalPrice,
+      currentUser,
+      cartItems,
+    });
+    // console.log(response);
+    dispatch({
+      type: "PLACE_ORDER_SUCCESS",
+      //   payload: response.data,
+    });
+    console.log(response);
+  } catch (error) {
+    dispatch({
+      type: "PLACE_ORDER_FAILED",
+      //   payload: error,
+    });
+    console.log(error);
+  }
+};
