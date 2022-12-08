83a14d91ea35de84038b88cfa5070835c83fed3c
Vica Fresinsya <vicafresinsya@gmail.com>
2022-12-08 09:26:11 +0700

    up

--- frontend/src/container/script.js
+++ frontend/src/container/script.js
@@ -1,4 +1,4 @@
-// import React from 'react';
+//import React from 'react';
 // import ReactDOM from 'react-dom';
 // import $ from 'jquery';
 //   import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js'
---
+++ frontend/src/container/stand.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './css/stand.css'
+
+export const stand = () => {
+    return (
+        <div>stand</div>
+    )
+}
+
+function stand() {
+    return (
+        <div className='stand'>
+            <h1>hello</h1>
+        </div>
+    );
+}
