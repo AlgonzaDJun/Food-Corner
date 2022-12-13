da2cbc966786e3aaebf2af23494e5ed61949f78e
Vica Fresinsya <vicafresinsya@gmail.com>
2022-12-13 16:16:08 +0700

    total pesanan

--- frontend/src/container/Checkout.jsx
+++ frontend/src/container/Checkout.jsx
@@ -13,7 +13,7 @@ const Checkout = () => {
 
         <section
           className=""
-          style={{  marginTop: '90px' , marginBottom: '100px'}}>
+          style={{ marginTop: '90px', marginBottom: '100px' }}>
           <div className="container">
             <div className="row">
               <div className="col-lg-12">
@@ -27,11 +27,13 @@ const Checkout = () => {
                 </div>
               </div>
             </div>
-            <div class="card mx-auto w-50">
+            <div class="card mx-auto col-lg-5">
               <div class="card-body">
                 <h2 class="nama-pesan">Member : Vica Fresinsya</h2>
                 <h2 class="nama-pesan text-muted">Status : ready</h2>
-                <h6 class="horizon text-muted">Tgl : 12-06-12</h6>
+                <div className="stand-menu">
+                  <h6 class="text-muted mb-3">Tgl : 12-06-12</h6>
+                </div>
 
                 <table class="table table-borderless" >
                   <thead className="w-100">
@@ -59,13 +61,58 @@ const Checkout = () => {
                         <h6 className="menu mb-2 text-center" style={{ width: '4rem' }}>1</h6>
                       </td>
                       <td>
+                        <p className="harga text-center" style={{ width: '6rem', }}>20,000</p>
+                      </td>
+                      <td>
                         <p className="harga text-center" style={{ width: '3rem', }}>20,000</p>
+                      </td>
+                    </tr>
+                    <tr className="stand-menu">
+                      <td>
+                        <h6 className="menu mb-2 text-wrap" style={{ width: '7rem' }}>ayam geprek</h6>
+                      </td>
+                      <td>
+                        <h6 className="menu mb-2 text-center" style={{ width: '4rem' }}>1</h6>
+                      </td>
+                      <td className="harga text-center" >
+                        <p style={{ width: '6rem', }}>20,000</p>
                       </td>
                       <td>
                         <p className="harga text-center" style={{ width: '3rem', }}>20,000</p>
                       </td>
                     </tr>
                     <tr className="horizon">
+                      <td></td>
+                      <td></td>
+                      <td className="">
+                        <h6 className="text-muted text-end mt-1 text-wrap" style={{ width: '7rem' }}>Total Pesanan:</h6>
+                      </td>
+                      <td className="">
+                        <p className="text-danger">20.000</p>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td className="">
+                        <h6 className="stand text-muted">
+                          <i className="uil uil-store">Stand pepaya:</i>
+                        </h6>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <h6 className="menu mb-2 text-wrap" style={{ width: '7rem' }}>ayam geprek</h6>
+                      </td>
+                      <td>
+                        <h6 className="menu mb-2 text-center" style={{ width: '4rem' }}>1</h6>
+                      </td>
+                      <td>
+                        <p className="harga text-center" style={{ width: '3rem', }}>20,000</p>
+                      </td>
+                      <td>
+                        <p className="harga text-center" style={{ width: '3rem', }}>20,000</p>
+                      </td>
+                    </tr>
+                    <tr className="stand-menu">
                       <td>
                         <h6 className="menu mb-2 text-wrap" style={{ width: '7rem' }}>ayam geprek</h6>
                       </td>
@@ -100,42 +147,7 @@ const Checkout = () => {
                         <p className="harga text-center" style={{ width: '3rem', }}>20,000</p>
                       </td>
                     </tr>
-                    <tr className="horizon">
-                      <td>
-                        <h6 className="menu mb-2 text-wrap" style={{ width: '7rem' }}>ayam geprek</h6>
-                      </td>
-                      <td>
-                        <h6 className="menu mb-2 text-center" style={{ width: '4rem' }}>1</h6>
-                      </td>
-                      <td>
-                        <p className="harga text-center" style={{ width: '3rem', }}>20,000</p>
-                      </td>
-                      <td>
-                        <p className="harga text-center" style={{ width: '3rem', }}>20,000</p>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td className="">
-                        <h6 className="stand text-muted">
-                          <i className="uil uil-store">Stand pepaya:</i>
-                        </h6>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <h6 className="menu mb-2 text-wrap" style={{ width: '7rem' }}>ayam geprek</h6>
-                      </td>
-                      <td>
-                        <h6 className="menu mb-2 text-center" style={{ width: '4rem' }}>1</h6>
-                      </td>
-                      <td>
-                        <p className="harga text-center" style={{ width: '3rem', }}>20,000</p>
-                      </td>
-                      <td>
-                        <p className="harga text-center" style={{ width: '3rem', }}>20,000</p>
-                      </td>
-                    </tr>
-                    <tr className="horizon">
+                    <tr className="stand-menu">
                       <td>
                         <h6 className="menu mb-2 text-wrap" style={{ width: '7rem' }}>ayam geprek</h6>
                       </td>
