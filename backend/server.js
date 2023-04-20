4b9a63a04d8b6ab573edd67b703963d40f4ce9fd
AlgonzaDJun <algonza.20050@mhs.unesa.ac.id>
2023-04-20 08:48:51 +0700

    change port server

--- backend/server.js
+++ backend/server.js
@@ -53,7 +53,7 @@ app.get("/", (req, res) => {
   res.send("Server Working... ");
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () =>
   console.log(`Server running in http://localhost:${port}`)
